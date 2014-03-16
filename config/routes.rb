995f642b0619fc98757995e614a7118b4f715a8b
Patrick Perey <the4dpatrick@yahoo.com>
2014-03-15 21:57:10 -0700

    Put to Patch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks do
     get 'completed', on: :collection
-    put 'complete', on: :member
+    patch 'complete', on: :member
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
